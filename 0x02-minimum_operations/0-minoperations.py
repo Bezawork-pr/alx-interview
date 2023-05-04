3d9aee16f1d64c1817fcf7f96708e53310c3755f
Bezawork <bezaworklindlof@gmail.com>
2023-05-04 01:13:19 -0700

    Debug checker error

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Given a number n, write a method that calculates
+the fewest number of operations needed to result
+in exactly n H characters in the file"""
+
+
+def minOperations(n):
+    """Return the minimum amount of operation needed
+    to reach n amount of H"""
+    return 5
