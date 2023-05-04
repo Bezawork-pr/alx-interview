5129234bdf38f6c30fd99d93c590e34909c1b52e
Bezawork <bezaworklindlof@gmail.com>
2023-05-04 03:17:01 -0700

    Fix unknown bug

--- 0x02-minimum_operations/another.py
+++ 0x02-minimum_operations/another.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-"""write a method that calculates the fewest number
-of operations needed to result in exactly
-n H characters in the file"""
+"""Given a number n, write a method that calculates
+the fewest number of operations needed to result
+in exactly n H characters in the file"""
 
 
 def minOperations(n):
-    """Calculate how many operation is needed to get to n
-    number of Hs"""
-    num = 1
-    num_of_operations = 0
-    cp_paste_operation = 2
-    paste_operation = 1
-    flag = 1
-    i = 0
-    count = 0
-    increment: List = [1, 3, 6, 12]
-    if (type(n) != int):
-        raise TypeError('Expected int')
-    if n == 0:
+    """Return the minimum amount of operation needed
+    to reach n amount of H"""
+    num, num_of_operations, cp_paste_operation = 1, 0, 2
+    paste_operation, flag, i, count = 1, 1, 0, 0
+    increment = [1, 3, 6, 12]
+    if n <= 0:
         return 0
     while(num != n and num < n):
         count += 1
         if count == 50:
             return 0
         if flag == 1 and n - num > increment[i]:
+            flag = 0
             if num_of_operations == 0:
                 num_of_operations = cp_paste_operation
                 num = num + increment[i]
             else:
                 num_of_operations = num_of_operations + cp_paste_operation
+                print(num_of_operations)
                 if (n - num > i):
                     i = i + 1
                 num = num + increment[i]
-            flag = 0
+        elif flag == 0:
+            print('What is going on bug')
         else:
+            print('Do u get here')
             num = num + increment[i]
             num_of_operations = num_of_operations + paste_operation
+            print(num_of_operations)
             flag = 1
-    return num_of_operations
+        return num_of_operations
+
