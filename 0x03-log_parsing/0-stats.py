9486fad7a0dd8a4f5b53aa5a02dc20848268839e
Bezawork <bezawork-pr@gmail.com>
2023-05-11 04:58:51 -0700

    Debug checker fail

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,24 +5,25 @@ and computes metrics
 """
 import sys
 
-total = 0
-current = 10
-add_file_size = 0
+if __name__ == "__main__":
+    total = 0
+    current = 10
+    add_file_size = 0
 
-s_repeat = {"200": 0, "301": 0, "400": 0, "401": 0,
-            "403": 0, "404": 0, "405": 0, "500": 0}
-for line in sys.stdin:
-    log_list = line.split(" ")
-    dict_values = list(s_repeat.values())
-    total = sum(dict_values)
-    if total == current:
-        print("File size: {}".format(add_file_size))
-        sorted_dict = sorted(s_repeat.items())
-        for key, value in sorted_dict:
-            if s_repeat[key] != 0:
-                print("{}: {}".format(key, value))
-        current += 10
-    else:
-        total = 0
-    s_repeat[log_list[-2]] += 1
-    add_file_size += int(log_list[-1])
+    s_repeat = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+    for line in sys.stdin:
+        log_list = line.split(" ")
+        dict_values = list(s_repeat.values())
+        total = sum(dict_values)
+        if total == current:
+            print("File size: {}".format(add_file_size))
+            sorted_dict = sorted(s_repeat.items())
+            for key, value in sorted_dict:
+                if s_repeat[key] != 0:
+                    print("{}: {}".format(key, value))
+            current += 10
+        else:
+            total = 0
+        s_repeat[log_list[-2]] += 1
+        add_file_size += int(log_list[-1])
