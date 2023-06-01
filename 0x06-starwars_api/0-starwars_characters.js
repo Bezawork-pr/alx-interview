4b7d45c0d40d52d686089fe378757a0f81df4ae3
Bezawork <bezaworkalx@gmail.com>
2023-06-01 10:56:58 +0000

    Fix semistandard errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,22 @@
 #!/usr/bin/node
 /* Write a script that prints all characters of a Star Wars movie */
 const request = require('request');
-const ip = "https://swapi-api.alx-tools.com/api/films/";
-const movie_id = process.argv[2];
-count = 0
-request(ip + movie_id, (error, response, body) => {
-  const character_ip = JSON.parse(body).characters
-  for (let i = 0; i < character_ip.length; i++) {
-    request(character_ip[i], (aerror, aresponse, abody) => {
-      const get_name = JSON.parse(abody).name;
-      console.log(JSON.parse(abody).name)
+const ip = 'https://swapi-api.alx-tools.com/api/films/';
+const movieId = process.argv[2];
+request(ip + movieId, (error, response, body) => {
+  if (error) return console.error(error);
+  const characterIp = JSON.parse(body).characters;
+  getCharacters(characterIp);
+});
+
+function getCharacters (characterIp) {
+  for (let i = 0; i < characterIp.length; i++) {
+    Promise(function (resolve, reject) {
+      request(characterIp[i], (error, response, body) => {
+        if (error) return console.error(error);
+        const getName = JSON.parse(body).name;
+        console.log(getName);
+      });
     });
-  } 
-}); 
-
-
+  }
+}
