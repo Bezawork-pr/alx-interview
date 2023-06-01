75e525f648fc91aa23e34558154c746791808340
Bezawork <bezaworkalx@gmail.com>
2023-06-01 11:09:26 +0000

    Print in ordered list as obtained from characters links

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,20 +3,17 @@
 const request = require('request');
 const ip = 'https://swapi-api.alx-tools.com/api/films/';
 const movieId = process.argv[2];
-request(ip + movieId, (error, response, body) => {
+request(ip + movieId, async function (error, response, body) {
   if (error) return console.error(error);
   const characterIp = JSON.parse(body).characters;
-  getCharacters(characterIp);
-});
-
-async function getCharacters (characterIp) {
   for (let i = 0; i < characterIp.length; i++) {
     await new Promise(function (resolve, reject) {
       request(characterIp[i], (error, response, body) => {
         if (error) return console.error(error);
         const getName = JSON.parse(body).name;
         console.log(getName);
+        resolve();
       });
     });
   }
-}
+});
