dc116f90b40a458e29bec4118ded4d5324be2365
Bezawork <bezaworkalx@gmail.com>
2023-06-14 02:41:03 -0700

    Determine the fewest number of coins needed to meet a given amount total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Determine the fewest number of coins
+needed to meet a given amount total"""
+
+
+def makeChange(coins, total):
+    """Determine the fewest number
+    of coins needed to meet
+    a given amount total"""
+    if total == 0 or total < 0:
+        return 0
+    num_of_coins = 0
+    while (total > 0):
+        largest = coins[0]
+        for i in range(len(coins)):
+            if coins[i] > largest and coins[i] <= total:
+                largest = coins[i]
+            if coins[i] > total or i == len(coins) - 1:
+                total -= largest
+                num_of_coins += 1
+                break
+    if total == 0:
+        return num_of_coins
+    return -1
