98c721760793ead604ab3367ab58c12af7a1b1fc
Bezawork <bezaworkalx@gmail.com>
2023-06-29 10:25:35 -0700

    Fix pycodestyle errors

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -27,36 +27,31 @@ def isWinner(x, nums):
         whose_turn = "Maria"
         num = nums[count]
         if len(obj['list_' + str(num)]) == 1:
-            player_ben_score += 1   
+            player_ben_score += 1
         while(len(obj['list_' + str(num)]) > 1):
-            
-            
             popping_mulitple = []
-            
             for i in range(1, num + 1):
-               
                 popping_num = obj['list_' + str(num)][1]
                 if i % popping_num == 0:
                     popping_mulitple.append(i)
-            
             for i in popping_mulitple:
                 obj['list_' + str(num)].remove(i)
-            
+
             if len(obj['list_' + str(num)]) == 1:
                 if whose_turn == "Maria":
                     player_maria_score += 1
                 else:
-                     player_ben_score += 1
+                    player_ben_score += 1
                 break
             if whose_turn == "Maria":
                 whose_turn = "Ben"
             else:
                 whose_turn = "Maria"
         count += 1
-            
+
     if player_maria_score > player_ben_score:
         return "Maria"
-    elif player_maria_score == player_ben_score: 
+    elif player_maria_score == player_ben_score:
         return None
     else:
         return "Ben"
